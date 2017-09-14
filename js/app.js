f5fb305e70a410e3715cb2e625d3297dd7b803c2
jonathan <jgcarlson@outlook.com>
2017-09-14 15:45:04 +0100

    added vanilla typing functionality to title

--- js/app.js
+++ js/app.js
@@ -1,4 +1,13 @@
+$(document).ready(function() {
+
+});
+
+
+
+
+
 $(document).foundation();
+
 $(document).on('click', 'a[href^="#"]', function(e) {
     // target element id
     var id = $(this).attr('href');
@@ -18,3 +27,18 @@ $(document).on('click', 'a[href^="#"]', function(e) {
     // animated top scrolling
     $('body, html').animate({scrollTop: pos});
 });
+
+// Vanilla Typing
+var sentence = "Hello. My name is Jonathan.";
+var element = document.getElementById('hello');
+var counter = 0;
+
+function type() {
+  element.innerHTML = element.innerHTML + sentence[counter];
+  counter++;
+  if (counter == sentence.length) {
+    clearInterval(interval);
+  }
+}
+
+var interval = setInterval(type, 100);
